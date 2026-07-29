af1eed2e5df65397f601988ade3bac02d64a93e4
David Hermida Perez <dhermidap@instradom.GRUPOABEIRAR.LOCAL>
2026-07-29 13:29:04 +0200

    first commit

---
+++ organizador.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# La carpeta que quiero ordenar (de momento, fija)
+carpeta = Path("carpeta_prueba")
+
+# Recorro cada elemento que hay dentro
+for archivo in carpeta.iterdir():
+    print(archivo)
